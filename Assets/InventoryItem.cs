ca7264b98590c16cba06da02ffdaad37cd490640
Tran Minh Quan <steammicharel@gmail.com>
2024-05-09 10:30:53 +0700

    Inventory UI, Drag and drop items

---
+++ Assets/InventoryItem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [Header("UI")]
+    public Image image;
+    [HideInInspector] public Item item;
+    [HideInInspector] public Transform parentAfterDrag;
+
+    public void InitialiseItem(Item newItem)
+    {
+        item = newItem;
+        image.sprite = newItem.image;
+    }
+    
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+    }
+}
---
+++ Assets/InventoryManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public InventorySlot[] inventorySlots;
+    public GameObject inventoryItemPrefab;
+    public void AddItem(Item item)
+    {
+        for(int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot != null) {
+                SpawnNewItem(item, slot);
+                return;
+            }
+        }
+    }
+
+    void SpawnNewItem(Item item, InventorySlot slot)
+    {
+        GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGo.GetComponentInChildren<InventoryItem>();
+        inventoryItem.InitialiseItem(item);
+    }
+}
