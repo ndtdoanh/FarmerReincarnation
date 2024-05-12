8645f489f65ad0718b7181c91e0e5738ffa56b1d
Tran Minh Quan <steammicharel@gmail.com>
2024-05-12 14:01:42 +0700

    Add TimeAgent., Growing Crops

--- Assets/Scripts/CropsManager.cs
+++ Assets/Scripts/CropsManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,20 +9,48 @@ using UnityEngine.UIElements;
 public class CropTile
 {
     public int growTimer;
+    public int growStage;
     public Crop crop;
+    public SpriteRenderer renderer;
 }
 
-public class CropsManager : MonoBehaviour
+public class CropsManager : TimeAgent
 {
     [SerializeField] TileBase plowed;
     [SerializeField] TileBase seeded;
     [SerializeField] Tilemap targetTilemap;
+    [SerializeField] GameObject cropsSpritePrefab;
 
     Dictionary<Vector2Int, CropTile> crops;
 
     private void Start()
     {
-        crops = new Dictionary<Vector2Int, CropTile> ();
+        crops = new Dictionary<Vector2Int, CropTile>();
+        onTimeTick += Tick;
+        Init();    
+    }
+
+    public void Tick()
+    {
+        foreach (CropTile cropTile in crops.Values)
+        {
+            if(cropTile.crop == null) { continue; } 
+            cropTile.growTimer += 1;
+
+            if(cropTile.growTimer >= cropTile.crop.growthStateTime[cropTile.growStage])
+            {
+                cropTile.renderer.gameObject.SetActive(true);
+                cropTile.renderer.sprite = cropTile.crop.sprites[cropTile.growStage];
+
+                cropTile.growStage += 1;
+            }
+
+            if(cropTile.growTimer >= cropTile.crop.timeToGrow)
+            {
+                Debug.Log("Done growing");
+                cropTile.crop = null;
+            }
+        }
     }
 
     public bool Check(Vector3Int position)
@@ -50,6 +79,12 @@ public class CropsManager : MonoBehaviour
     {
         CropTile crop = new CropTile();
         crops.Add((Vector2Int)position, crop);
+
+        GameObject go = Instantiate(cropsSpritePrefab);
+        go.transform.position = targetTilemap.CellToWorld(position);
+        go.transform.position -= Vector3.forward * 0.01f;
+        go.SetActive(false);    
+        crop.renderer = go.GetComponent<SpriteRenderer>();
 
         targetTilemap.SetTile(position, plowed);
     }
--- Assets/Scripts/DayTimeController.cs
+++ Assets/Scripts/DayTimeController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
@@ -8,6 +9,7 @@ using UnityEngine.UI;
 public class DayTimeController : MonoBehaviour
 {
     const float secondsInDay = 86400f;
+    const float phaseLength = 900f; //15m chunk of time
 
     [SerializeField] Color nightLightColor;
     [SerializeField] AnimationCurve nightTimeCurve;
@@ -16,7 +18,7 @@ public class DayTimeController : MonoBehaviour
     float time;
 
     [SerializeField] float timeScale = 60f;
-    
+    [SerializeField] float startAtTime = 28800f; //seconds
     [SerializeField] Text text;
     [SerializeField] Light2D globalLight;
     private int days;
@@ -30,19 +32,69 @@ public class DayTimeController : MonoBehaviour
     {
         get { return time % 3600f / 60f; }
     }
+
+    List<TimeAgent> agents;
+
+    private void Awake()
+    {
+        agents = new List<TimeAgent>();
+    }
+
+    private void Start()
+    {
+        time = startAtTime;
+    }
+
+    public void Subscribe(TimeAgent timeAgent)
+    {
+        agents.Add(timeAgent);
+    }
+
+    public void Unsubscribe(TimeAgent timeAgent)
+    {
+        agents.Remove(timeAgent);
+    }
+
     private void Update()
     {
         time += Time.deltaTime * timeScale;
-        int hh = (int)Hours;
-        int mm = (int)Minutes;
-        text.text = hh.ToString("00") + ":" + mm.ToString("00");
-        float v = nightTimeCurve.Evaluate(Hours);
-        Color c = Color.Lerp(dayLightColor, nightLightColor, v);
-        globalLight.color = c;
-        if(time > secondsInDay)
+        TimeValueCalculation();
+        DayLight();
+        if (time > secondsInDay)
         {
             NextDay();
         }
+
+        TimeAgents();
+    }
+
+
+    int oldPhase = 0;
+    private void TimeAgents()
+    {
+        int currentPhase = (int)(time / phaseLength);
+        if (oldPhase != currentPhase)
+        {
+            oldPhase = currentPhase;
+            for (int i = 0; i < agents.Count; i++)
+            {
+                agents[i].Invoke();
+            }
+        }
+    }
+
+    private void DayLight()
+    {
+        float v = nightTimeCurve.Evaluate(Hours);
+        Color c = Color.Lerp(dayLightColor, nightLightColor, v);
+        globalLight.color = c;
+    }
+
+    private void TimeValueCalculation()
+    {
+        int hh = (int)Hours;
+        int mm = (int)Minutes;
+        text.text = hh.ToString("00") + ":" + mm.ToString("00");
     }
 
     private void NextDay()
