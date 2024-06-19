0ac32e9a68b1a6d4fed939a6893b23cd8ccab5e7
Tran Minh Quan <steammicharel@gmail.com>
2024-06-19 21:24:55 +0700

    fix tileset all scene

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public string sceneName;
+    private bool isPaused = false;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void NextStage()
+    {
+        SceneManager.LoadSceneAsync(3);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+}
